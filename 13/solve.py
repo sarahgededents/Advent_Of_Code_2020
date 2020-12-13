46dc16f495bf3b6742908864a5afe4870feb275e
Sarah Neau Monier <sarah.neau-monier@epita.fr>
2020-12-13 16:44:46 +0100

    [ADD] better impl

--- 13/solve.py
+++ 13/solve.py
@@ -1,5 +1,6 @@
 import operator
 from math import prod
+from collections import OrderedDict
 
 def chinese_remainder(nums, rems):
     prod_N = prod(nums)
@@ -10,16 +11,12 @@ with open("input") as inp:
     inp = [line.rstrip().split() for line in inp]
     depart = int(inp[0][0])
     ids = [i.split(',') for i in inp[1]][0]
-    buses = {}
+    buses = OrderedDict()
     for i, id in enumerate(ids):
         if id != 'x':
             buses[i] = int(id)
 
-c = []
-bus_ids = buses.values()
-for id in bus_ids:
-    c.append(id - depart % id)
+c = [bid - depart % bid for bid in buses.values()]
 
-
-print("Part 1: ", min(c) * list(bus_ids)[c.index(min(c))])
-print("Part 2: ", chinese_remainder(buses.values(), map(operator.sub, list(bus_ids), list(buses.keys()))))
+print("Part 1: ", min(c) * list(buses.values())[c.index(min(c))])
+print("Part 2: ", chinese_remainder(buses.values(), map(operator.sub, list(buses.values()), list(buses.keys()))))
